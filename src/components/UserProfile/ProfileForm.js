92235439919109b2fb61b9c1e8244f13a1af90e2
HS-1000 <68994620+HS-1000@users.noreply.github.com>
2023-02-19 20:43:02 +0900

    FEAT: 프로필 수정기능 추가

--- src/components/UserProfile/ProfileForm.js
+++ src/components/UserProfile/ProfileForm.js
@@ -1,40 +1,62 @@
 import Information from "./Information";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
 const ProfileForm = props => {
-  //TODO 유저프로필 정보 가져오기 -> 이거는 Profile.js에서 구현하기
-  //TODO 수정기능(isDisabled)수정 on/off가능하도록 정의
   //TODO 카카오 연동하기 버튼 (카카오계정이 아니라면)
   //TODO 모든기능 완성후 css수정
-  //form에 포함될것 : profileImg, profileName, profileMessage
-  //TODO 하위 컴포넌트 state의 setting함수의 주소만 넘겨줘서 사용가능한지 확인하기
-  //^^^ 가능하다
   const [isDisabled, setIsDisabled] = useState(true);
-  const isDisabledHandler = () => { 
-    if (!isDisabled && isFormChange) {
-      
-    }
-    setIsDisabled(!isDisabled); 
-    //TODO 저장하기가 눌렸을때(isDisabled == false)
-    
-  };
+  const [profileId, setProfileId] = useState(props.profileId);
+  const [csrf, setCsrf] = useState('');
   const [isImageChange, setIsImageChange] = useState(false);
   const [isNameChange, setIsNameChange] = useState(false);
   const [isMessageChange, setIsMessageChange] = useState(false);
-  const [isFormChange, setIsFormChange] = useState(false);
-  useEffect(() => {
-    setIsFormChange(isImageChange || isNameChange || isMessageChange);
-    console.log(isFormChange);
-  }, [isImageChange, isNameChange, isMessageChange]);
-  
 
-  const return_result = <form action={`${props.URL}/main/`}>
+  const submitRef = useRef(null);
+  const isDisabledHandler = () => { 
+    if (
+      !isDisabled && 
+      (isNameChange || isImageChange || isMessageChange)) {
+      submitRef.current.submit();
+    } else {
+      setIsDisabled(false);
+      return undefined;
+    }
+    setIsDisabled(!isDisabled);     
+  };
+  const getCsrfToken = () => {
+    let csrfToken = "";
+    document.cookie.split('; ').map(element => {
+      const [name, ...value] = element.split("=");
+      if (name == "csrftoken") {
+        csrfToken = value;
+      }
+    }
+    );
+    return csrfToken;
+  }
+  useEffect(() => {
+    setCsrf(getCsrfToken());
+  }, []);
+
+  const return_result = 
+    <form 
+      action={`${props.URL}/main/UserProfile/${profileId}/update/`} 
+      ref={submitRef} 
+      method="POST"
+      data-method="PUT"
+      encType="multipart/form-data">
+    <input 
+      type="hidden" 
+      name="csrfmiddlewaretoken" 
+      id="form-csrftoken" 
+      value={csrf} />
     <Information 
     type="image"
     src={props.src}
     isChange={setIsImageChange}
     isDisabled={isDisabled}
+    formName="profileImg"
     />
     <Information 
     type="text"
@@ -42,6 +64,7 @@ const ProfileForm = props => {
     isDisabled={isDisabled}
     default={props.profileName}
     placeHolder="Name"
+    formName="profileName"
     />
     <Information 
     type="text"
@@ -49,10 +72,10 @@ const ProfileForm = props => {
     isDisabled={isDisabled}
     default={props.profileMessage}
     placeHolder="Status Message"
+    formName="profileMessage"
     />
     <hr />
     <button 
-      className={`isDisabled ${isFormChange ? '' : 'form-changed'}`} 
       onClick={isDisabledHandler}
       type="button">
       { isDisabled ? "수정하기" : "저장하기" }
@@ -63,7 +86,8 @@ const ProfileForm = props => {
 
 ProfileForm.defaultProps = {
   URL: "http://localhost:8000",
-  src: "http://localhost:8000/media/user_profile/default1.jpg"
+  src: "http://localhost:8000/media/user_profile/default1.jpg",
+  userId: 0
 }
 
 export default ProfileForm;

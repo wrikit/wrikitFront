2dc35ba69891f465ef35e6efae271c8863f499c5
WooriSunoo <cherry21004@gmail.com>
2023-03-02 12:48:34 +0900

    feat: documentlist link추가

--- src/components/DocumentPage/DocumentItem.js
+++ src/components/DocumentPage/DocumentItem.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { HiDocumentText } from "react-icons/hi";
 //documentItem컴포넌트 문서 번호, 이름, 마지막업데이트날짜
 const DocumentItem = (props) => {
@@ -8,14 +9,14 @@ const DocumentItem = (props) => {
   console.log("DocumentItem컴포넌트", document.id);
   //   post =>{id: x, title: xxx, body: xxx}
   return (
-    <div className="DocumentItem">
+    <Link className="DocumentItem" to={"/document/" + document.id}>
       <div>
         <HiDocumentText />
         <span className="id">No. {document.id}</span>
         <span className="docName">- {document.docName}</span>
         <span className="lastUpdate">- {document.lastUpdate}</span>
       </div>
-    </div>
+    </Link>
   );
 };
 

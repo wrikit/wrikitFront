34c21d25a082a072588d19921682f7d16e057f7c
uj-kim <ninakyj@gmail.com>
2023-04-03 22:07:20 +0900

    mypage에 로그아웃 버튼 추가

--- src/components/UserProfile/Profile.js
+++ src/components/UserProfile/Profile.js
@@ -58,7 +58,28 @@ const Profile = (props) => {
     };
     getData();
   }, []);
-
+  //로그아웃 기능
+  const onLogout = () => {
+    axios
+      .post("http://localhost:8000/auth/logout/", {}, { withCredentials: true })
+      .then((res) => {
+        // if (Kakao.Auth.getAccessToken()) {
+        //   Kakao.API.request({
+        //     url: '/v1/user/unlink',
+        //     success: response => {
+        //       console.log(response);
+        //     },
+        //     fail: error => {
+        //       console.log(error);
+        //     },
+        //   })
+        //   Kakao.Auth.setAccessToken(undefined);
+        // }
+      })
+      .then(() => {
+        document.location.href = "/";
+      });
+  };
   return (
     <div className={props.type} id="profile">
       {name && isKakao != undefined ? (
@@ -71,6 +92,7 @@ const Profile = (props) => {
             profileId={profileId}
           />
           {isKakao ? "" : <PassSetting />}
+          <button onClick={onLogout}>로그아웃</button>
         </div>
       ) : (
         <div>Loading...</div>
--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,36 +1,33 @@
-import React, {useRef, useState, useEffect} from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "../styles/Mypage.scss";
 import Profile from "../components/UserProfile/Profile";
 
-const Mypage=(props)=>{
-    
-    //다른 곳 클릭시 마이페이지 닫힘
-    const setIsSidebarOpen= props.setIsSidebarOpen;
-    let mypageRef = useRef();
-    
-    useEffect(() => {
-        let handler = (e) => {
-            if(!mypageRef.current.contains(e.target)){
-                setIsSidebarOpen(false);
-            }
-        };
+const Mypage = (props) => {
+  //다른 곳 클릭시 마이페이지 닫힘
+  const setIsSidebarOpen = props.setIsSidebarOpen;
+  let mypageRef = useRef();
+
+  useEffect(() => {
+    let handler = (e) => {
+      if (!mypageRef.current.contains(e.target)) {
+        setIsSidebarOpen(false);
+      }
+    };
 
     document.addEventListener("mousedown", handler);
 
     return () => {
       document.removeEventListener("mousedown", handler);
     };
-  },[]);
+  }, []);
 
-
-    return(
-    <div className="Mypage" ref={mypageRef} >
-        <Profile type="side"></Profile>
-        <button onClick={props.onCloseClick}>Close</button>
-        {/* <button onClick={MypageClick}>Close</button> */}
-    <p>This is Mypage</p>
+  return (
+    <div className="Mypage" ref={mypageRef}>
+      <button onClick={props.onCloseClick}>Close</button>
+      <Profile type="side"></Profile>
+      {/* <button onClick={MypageClick}>Close</button> */}
     </div>
-    )
-}
+  );
+};
 
 export default Mypage;

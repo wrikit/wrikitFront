a9b1831d4b8618f7314267a34593637595b77879
uj-kim <ninakyj@gmail.com>
2023-04-18 18:39:26 +0900

    quillEditor ToolBar에 이미지 아이콘 추가(기능X)

--- src/components/TextEditor/QuillEditor.js
+++ src/components/TextEditor/QuillEditor.js
@@ -1,17 +1,15 @@
-import React, {useState, useEffect} from 'react';
-import ReactQuill from 'react-quill';
-import Quill from 'quill';
-import 'react-quill/dist/quill.snow.css';
+import React, { useState, useEffect } from "react";
+import ReactQuill from "react-quill";
+import Quill from "quill";
+import "react-quill/dist/quill.snow.css";
 
-
-const QuillEditor = props => {
+const QuillEditor = (props) => {
   const [content, setContent] = useState(props.data);
 
-  const changeHandler = value => {
+  const changeHandler = (value) => {
     setContent(value);
-  }
+  };
 
-  
   // const Link = Quill.import('formats/link');
 
   // Link.sanitize = function(url) {
@@ -23,37 +21,66 @@ const QuillEditor = props => {
 
   // Quill.register(Link, true);
   const toolBar = [
-    [{ 'font': [] }, {size: []}, {color: []}, {'list': 'ordered'}, {'list': 'bullet'}],
-    ['bold', 'italic', 'underline', 'strike', 'blockquote', 'link', { 'align': [] }, {'indent': '+1'}]
+    [
+      { font: [] },
+      { size: [] },
+      { color: [] },
+      { list: "ordered" },
+      { list: "bullet" },
+    ],
+    [
+      "bold",
+      "italic",
+      "underline",
+      "strike",
+      "blockquote",
+      "link",
+      "image",
+      { align: [] },
+      { indent: "+1" },
+    ],
   ];
   const modules = {
     toolbar: toolBar,
     clipboard: {
       // toggle to add extra line breaks when pasting HTML:
       matchVisual: false,
-    }
+    },
   };
 
   const formats = [
-    'font', 'size', 'color', '',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet', 'link', 'indent', 'align'
+    "font",
+    "size",
+    "color",
+    "",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "link",
+    "image",
+    "indent",
+    "align",
   ];
 
   return (
     <div>
-      <ReactQuill 
-        value={content} 
+      <ReactQuill
+        value={content}
         onChange={changeHandler}
         modules={modules}
         readOnly={props.type == "reader"}
-        formats={formats} />
+        formats={formats}
+      />
     </div>
   );
 };
 
 QuillEditor.defaultProps = {
-  data: '',
-}
+  data: "",
+};
 
 export default QuillEditor;

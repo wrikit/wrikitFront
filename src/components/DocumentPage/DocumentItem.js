d42700a83e331c495570c5c47809a147324d2171
uj-kim <ninakyj@gmail.com>
2023-04-29 14:12:49 +0900

    design : 삭제 아이콘 추가

--- src/components/DocumentPage/DocumentItem.js
+++ src/components/DocumentPage/DocumentItem.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { HiDocumentText } from "react-icons/hi";
 import "../../styles/DocumentItem.scss";
+import { MdDeleteForever } from "react-icons/md";
 import axios from "axios";
 import { useEffect, useState } from "react";
 //documentItem컴포넌트 문서 번호, 이름, 마지막업데이트날짜
@@ -14,7 +15,10 @@ const DocumentItem = (props) => {
       <span className="DocumentItem__checkbox">
         <input type="checkbox" />
       </span>
-      <Link className="DocumentItem__container" to={"/document/" + document.id + "/editor"}>
+      <Link
+        className="DocumentItem__container"
+        to={"/document/" + document.id + "/editor"}
+      >
         <div className="DocumentItems">
           <span className="DocumentItems__icon">
             <HiDocumentText size={24} />
@@ -22,6 +26,9 @@ const DocumentItem = (props) => {
           <span className="DocumentItems__id">No. {document.id}</span>
           <span className="DocumentItems__docName">{document.docName}</span>
           <span className="DocumentItems__docDate">{document.date}</span>
+          <button className="DocumentItems__button">
+            <MdDeleteForever size={24} />
+          </button>
         </div>
       </Link>
     </div>

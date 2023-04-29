fa8f0f2a83b413517128dac6009ce44e97e42a86
uj-kim <ninakyj@gmail.com>
2023-04-29 15:45:22 +0900

    design : documentItem에 hover했을 때 삭제 아이콘 보이게 수정

--- src/components/DocumentPage/DocumentItem.js
+++ src/components/DocumentPage/DocumentItem.js
@@ -7,11 +7,38 @@ import { useEffect, useState } from "react";
 //documentItem컴포넌트 문서 번호, 이름, 마지막업데이트날짜
 const DocumentItem = (props) => {
   //   console.log(props);
+  // const handleHover = () => {
+  //   const delBtn = document.getElementsByClassName("DocumentItem__button");
+  //   delBtn.style.display = "flex";
+  // };
+  // const handleMouseOut = () => {
+  //   const delBtn = document.getElementsByClassName("DocumentItem__button");
+  //   delBtn.style.display = "none";
+  // };
   const { document } = props;
   console.log("DocumentItem컴포넌트key", document);
+  // const deleteDocument = () => {
+  //   axios
+  //     .post(
+  //       "http://localhost:8000/main/delete-document/",
+  //       { documentid: document.id },
+  //       { withCredentials: true }
+  //     )
+  //     .then((res) => {
+  //       console.log(res.data);
+  //       if (res.data.result) {
+  //         alert("삭제완료");
+  //         window.location.href = "/document";
+  //       }
+  //     });
+  // };
 
   return (
-    <div className="DocumentItem">
+    <div
+      className="DocumentItem"
+      // onMouseOver={handleHover}
+      // onMouseOut={handleMouseOut}
+    >
       <span className="DocumentItem__checkbox">
         <input type="checkbox" />
       </span>
@@ -26,11 +53,11 @@ const DocumentItem = (props) => {
           <span className="DocumentItems__id">No. {document.id}</span>
           <span className="DocumentItems__docName">{document.docName}</span>
           <span className="DocumentItems__docDate">{document.date}</span>
-          <button className="DocumentItems__button">
-            <MdDeleteForever size={24} />
-          </button>
         </div>
       </Link>
+      <button className="DocumentItem__button">
+        <MdDeleteForever size={24} />
+      </button>
     </div>
   );
 };

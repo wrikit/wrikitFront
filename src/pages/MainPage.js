35312aec7958fe847d5b3eb9595d58fec628299a
uj-kim <ninakyj@gmail.com>
2023-05-15 23:04:52 +0900

    design : mainpage 서비스 소개 디자인 및 carousel 추가

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,9 +1,24 @@
 import { NavLink } from "react-router-dom";
 import "../styles/MainPage.scss";
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const MainPage = (isLogin) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 4000, // 이미지가 바뀌는 속도
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true, // 자동 슬라이드
+    autoplaySpeed: 5000, // 다음 이미지로 넘어가는 시간
+    pauseOnHover: false, // 마우스 호버시 자동 슬라이드 멈춤 여부
+    arrows: true,
+  }
   return (
     <main className="MainPage">
+      <Slider {...settings} >
       <div className="banner index-img">
         <div className="main__textwrapper">
           <h1 className="main__textwrapper__title">어디든, 언제든!<br /> 간편한 문서공유</h1>
@@ -31,6 +46,28 @@ const MainPage = (isLogin) => {
           </div>
         </div>
       </div>
+      <div className="second-banner">
+        <div className="second-img">
+          <div className="second-textwrapper">
+            이메일 초대 없이 링크만으로 간편한 공유!
+            <div className="main__textwrapper__btnArea">
+            {isLogin ? (
+                <NavLink to="/document">
+                <button className="runBtn">
+                  <span>시작하기</span>
+                </button>
+              </NavLink>
+            ) : (  <NavLink to="/lgpage">
+            <button className="runBtn">
+              <span>시작하기</span>
+            </button>
+          </NavLink>)}
+          
+          </div>
+          </div>
+        </div>
+      </div>
+      </Slider>
     </main>
   );
 };
